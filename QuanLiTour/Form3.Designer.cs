f5aaab1195448842f917c8262cf5abb41c03a99b
lethanhtuyen <lethanhtuyen10081998@gmail.com>
2019-10-12 14:57:41 +0700

    no message

--- QuanLiTour/Form3.Designer.cs
+++ QuanLiTour/Form3.Designer.cs
@@ -29,7 +29,6 @@
         private void InitializeComponent()
         {
             this.button1 = new System.Windows.Forms.Button();
-            this.textBoxX1 = new DevComponents.DotNetBar.Controls.TextBoxX();
             this.SuspendLayout();
             // 
             // button1
@@ -42,25 +41,11 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             // 
-            // textBoxX1
-            // 
-            // 
-            // 
-            // 
-            this.textBoxX1.Border.Class = "TextBoxBorder";
-            this.textBoxX1.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
-            this.textBoxX1.Location = new System.Drawing.Point(128, 83);
-            this.textBoxX1.Name = "textBoxX1";
-            this.textBoxX1.PreventEnterBeep = true;
-            this.textBoxX1.Size = new System.Drawing.Size(100, 20);
-            this.textBoxX1.TabIndex = 1;
-            // 
             // Form3
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(436, 206);
-            this.Controls.Add(this.textBoxX1);
             this.Controls.Add(this.button1);
             this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form3";
@@ -72,6 +57,5 @@
         #endregion
 
         private System.Windows.Forms.Button button1;
-        private DevComponents.DotNetBar.Controls.TextBoxX textBoxX1;
     }
 }
