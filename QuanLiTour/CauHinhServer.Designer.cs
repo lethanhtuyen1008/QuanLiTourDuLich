dc33fa67ae7b9be20b3ea1dd71f820fea22627ee
lethanhtuyen <lethanhtuyen10081998@gmail.com>
2019-10-14 18:42:18 +0700

    form server

--- QuanLiTour/CauHinhServer.Designer.cs
+++ QuanLiTour/CauHinhServer.Designer.cs
@@ -1,13 +1,13 @@
 ﻿namespace QuanLiTour
 {
-    partial class FormMain
+    partial class CauHinhServer
     {
-        /// <summary>
+        /// <summary> 
         /// Required designer variable.
         /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
+        /// <summary> 
         /// Clean up any resources being used.
         /// </summary>
         /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
@@ -20,47 +20,26 @@
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
+        #region Component Designer generated code
 
-        /// <summary>
-        /// Required method for Designer support - do not modify
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
         /// the contents of this method with the code editor.
         /// </summary>
         private void InitializeComponent()
         {
-            this.login1 = new QuanLiTour.Login();
             this.SuspendLayout();
             // 
-            // login1
-            // 
-            this.login1.Location = new System.Drawing.Point(25, 34);
-            this.login1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.login1.Name = "login1";
-            this.login1.Size = new System.Drawing.Size(473, 216);
-            this.login1.TabIndex = 0;
-            // 
-            // FormMain
+            // CauHinhServer
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(536, 317);
-            this.Controls.Add(this.login1);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
-            this.Name = "FormMain";
-            this.Text = "Form1";
-            this.Load += new System.EventHandler(this.FormMain_Load);
+            this.Name = "CauHinhServer";
+            this.Size = new System.Drawing.Size(512, 208);
             this.ResumeLayout(false);
 
         }
 
         #endregion
-
-        private Login login1;
-
-
-
-
-
     }
 }
-
